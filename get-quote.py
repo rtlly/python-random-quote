07b047e4310e8c96357ace420f1310ed6bf7664f
liyu <yszli@thoughtworks.com>
2021-04-06 13:43:55 +0800

    add random

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,15 @@
+import random
+
 def primary():
   print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
-  print(quotes)
-  print(quotes[0])
+  last = 13
+  ran = random.randint(0, last)
+  
+  print(quotes[ran])
 
 if __name__== "__main__":
   primary()
